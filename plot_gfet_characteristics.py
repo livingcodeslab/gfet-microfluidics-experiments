d486763be2d40f77d64989fd92272cd643c4bf78
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2025-03-28 15:51:23 -0500

    Allow user to change title and axes labels.

--- plot_gfet_characteristics.py
+++ plot_gfet_characteristics.py
@@ -18,6 +18,8 @@ logger = logging.getLogger(__name__)
 def plot_files(args: Namespace):
     g = gnuplot.Gnuplot(log=True)
 
+    _ylabel = "R (KΩ)" if args.y_axis == "drain_resistance" else "I_D (A)"
+
     # Set plotting style
     g.set(terminal='svg font "arial,10" fontscale 1.0 size 1200,1000 dynamic background rgb "white"',
           #terminal='pngcairo font "arial,10" fontscale 1.0 size 1000, 800',
@@ -25,7 +27,7 @@ def plot_files(args: Namespace):
           output=f'"{args.plot_filename}"',
           key="fixed left top horizontal Right noreverse enhanced autotitle box lt black linewidth 1.000 dashtype solid",
           # samples="50, 50",
-          title='"GFET Characteristics" font ",20" textcolor lt -1 norotate',
+          title=f'"{args.plot_title}" font ",20" textcolor lt -1 norotate',
           datafile='separator ","',
           # xtics=0.005,
           xrange='[* : *] noreverse writeback',
@@ -36,15 +38,15 @@ def plot_files(args: Namespace):
           cbrange='[* : *] noreverse writeback',
           rrange='[* : *] noreverse writeback',
           colorbox='vertical origin screen 0.9, 0.2 size screen 0.05, 0.6 front noinvert bdefault',
-          xlabel='"V_D (V)"',
-          ylabel='"R (KΩ)" rotate')
+          xlabel='"V_G (V)"',
+          ylabel=f'"{_ylabel}" rotate')
 
     g.cmd("NO_ANIMATION = 1")
 
     _range, _len = range_length(float_range(*args.range))
     plot_args = [
         (f"'{build_filename(args.input_directory, args.file_prefix, idx, _len)}' "
-         "using 'x_axis':'drain_resistance' "
+         f"using 'x_axis':'{args.y_axis}' "
          f"title 'Gate Voltage {gate_voltage:0.3}V' "
          "with lines")
         for idx, gate_voltage in enumerate(_range, start=1)]
@@ -70,6 +72,11 @@ def main():
         help=("A comma-separated list of 2 or 3 float values. "
               "If 3 values are provided, the third is used as a step. "
               "If only 2 values are provided, the default step is 0.1"))
+    parser.add_argument(
+        "--plot-title", type=str, default="GFET Characteristics",
+        help="Title of the plot.")
+    parser.add_argument("--x-axis", type=str, default="gate_voltage")
+    parser.add_argument("--y-axis", type=str, default="drain_current")
     parser.add_argument(
         "--log-level", type=str, default="info",
         choices=("critical", "error", "warning", "info", "debug"))
