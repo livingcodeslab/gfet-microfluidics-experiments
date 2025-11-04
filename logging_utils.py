f42f7db05063a6f67758745582a31a956fee59b9
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2025-11-04 12:08:14 -0600

    Add function to setup loggers consistently.

--- logging_utils.py
+++ logging_utils.py
@@ -1,6 +1,23 @@
 """Utilities to help with logging."""
 import logging
 
+
 def set_loggers_level(loggers: tuple[str, ...], loglevel):
+    """Set the log level for specified loggers."""
     for logger_str in loggers:
         logging.getLogger(logger_str).setLevel(loglevel)
+
+
+def setup_logging(
+        loglevel: str,
+        logger: logging.Logger,
+        aux_loggers: tuple[str, ...]
+):
+    """Setup logging in a most generic way."""
+    logging.basicConfig(
+        encoding="utf-8",
+        format="%(asctime)s - %(name)s - %(levelname)s — %(message)s",
+        loglevel=logging.INFO)
+    logger.setLevel(getattr(logging, loglevel.upper()))
+    set_loggers_level(aux_loggers, logger.getEffectiveLevel())
+    return logger
