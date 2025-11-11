88a3f9cc58553972c5f2257ff95bef5a0f3feeed
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2025-11-11 04:54:51 -0600

    CLI: add utility for adding logging argument to CLI scripts.

--- gfet/cli.py
+++ gfet/cli.py
@@ -1,4 +1,6 @@
 """Command-line utilities"""
+from argparse import ArgumentParser
+
 
 def fetch_range_float(value: str) -> tuple[float, float, float]:
     """Convert a string into a tuple of values."""
@@ -13,3 +15,18 @@ def fetch_range_float(value: str) -> tuple[float, float, float]:
     if items[0] < items[1]:
         return items
     return (items[1], items[0], items[2])
+
+
+def cli_add_logging_arg(parser: ArgumentParser) -> ArgumentParser:
+    """Add --log-level option to the CLI"""
+    parser.add_argument(
+        "--log-level",
+        "--loglevel",
+        type=str,
+        choices=("critical",
+                 "error",
+                 "warning",
+                 "info",
+                 "debug"),
+        default="info")
+    return parser
